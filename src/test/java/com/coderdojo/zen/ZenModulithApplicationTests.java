cb6ce2f48b2b79c15c8a587b1e9dc76c5345841d
anthonygnolan <anthonygnolan@icloud.com>
2025-03-22 20:06:16 +0000

    Add template files

---
+++ src/test/java/com/coderdojo/zen/ZenModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderdojo.zen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenModulithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
